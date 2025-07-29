c12ab87db9b49eb97c6ac9706debebcbddd794d5
NgangaKamau3 <ngangajohn23405@gmail.com>
2025-07-29 05:56:04 +0300

    remove: D+ level code - replaced with production-ready architecture

--- optimizers/__init__.py
+++
@@ -1,7 +0,0 @@
-from .sgd import SGD
-from .adam import Adam
-from .rmsprop import RMSProp
-from .lbfgs import LBFGS
-from .base import BaseOptimizer
-
-__all__ = ["SGD", "Adam", "RMSProp", "LBFGS", "BaseOptimizer"]
--- test_functions/__init__.py
+++
@@ -1,7 +0,0 @@
-from .rosenbrock import Rosenbrock
-from .rastrigin import Rastrigin
-from .sphere import Sphere
-from .beale import Beale
-from .base import BaseFunction
-
-__all__ = ["Rosenbrock", "Rastrigin", "Sphere", "Beale", "BaseFunction"]
